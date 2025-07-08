431a831352d72f71454af814ba9eea784c29a1a2
David Baldie <daivd.1138@gmail.com>
2025-07-07 22:00:28 -0700

    Add an example of The Magi working proactively

--- services/orchestrator/src/magi_manifesto.ts
+++ services/orchestrator/src/magi_manifesto.ts
@@ -48,3 +48,37 @@ convince them to change their initial position, or whether they will adjust thei
 This process will continue until a consensus is reached or a predetermined number of rounds has
 been reached (this is known as an IMPASSE). In either case, a summary of the discussion will
 be presented to the user.`;
+
+export const MAGI_EXAMPLE = `On a timer, the Magi system is triggered and the three Magi are
+activated. They load their shared memory and initialization data.
+
+Without any query, they set about checking if there is any news, data, or events that
+might be relevant to the user. Balthazar, with internet access, checks the weather in the area and
+sees that there is 100% chance of snow in the next 24 hours. Melchior, with access to the user's
+calendar, sees that the user has a flight to catch at 8:00 AM today which is in 4 hours. Caspar,
+with access to the home automation system, sees that the user has a car parked outside and the
+user is still sleeping.
+
+Balthazar's independent analysis was that the user should be prepared for snow.
+Melchior's independent analysis was that the user should leave for the airport no later than
+6:00 AM to ensure they arrive on time. Caspar's independent analysis resulted in no advice or
+suggestions as everything is normal.
+
+Once the three Magi share their independent analyses, they enter the deliberation phase.
+Due to the randomized nature of the order, Melchior is the first to respond to all the findings.
+Melchior acknowledges Balthazar's warning and decides to adjust her advice to wake up earlier
+than 6:00 AM, suggesting 5:30 AM to allow for snow preparation. Balthazar, who is next, compares the
+current time with the new suggested wake-up time and suggests Caspar set their next Magi wake-up
+time alarm for 5:30 AM. Caspar agrees with this and sets the next Magi-wake-up time alarm for 5:30 AM.
+This event is catalogued in the shared memory.
+
+When the magi wake up at 5:30 AM, they repeat the process, and speak to the user: "We apologize
+for waking you up, but we have determined that you should wake up now to prepare for snow conditions
+and leave for the airport by 6:00 AM for your 8:00AM departure."
+
+It was determined that they will set the next wakeup time for 5:45 AM, which is in another 15 minutes,
+to nudge the user if Caspar detects the user is still sleeping. If it is determined that the user
+is awake and has left by the suggested time, the Magi consider this task completed successfully.
+Upon a completion of a task (succcessful or otherwise) they will hold a brief retrospective
+to discuss how they could improve their process or settings in the future.
+`
